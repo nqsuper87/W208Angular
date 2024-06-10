2bf48cf3dace6720c756893c9c261a2c57664757
nqsuper87 <69568653+nqsuper87@users.noreply.github.com>
2024-06-10 22:12:02 +0700

    Update firebase.config.ts

--- src/app/firebase.config.ts
+++ src/app/firebase.config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyDyEYeUM82djy7pSDW0a-q3fyL6m6LCjS0",
-    authDomain: "angular4-b1ce8.firebaseapp.com",
-    projectId: "angular4-b1ce8",
-    storageBucket: "angular4-b1ce8.appspot.com",
-    messagingSenderId: "1094707080431",
-    appId: "1:1094707080431:web:404b1082f8ca194702bf1a"
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
   };
